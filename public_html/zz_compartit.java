e8eb895d517db8873fdb3b96941dd9bbd609f03b
fgrange <fgrange@DESKTOP-IIQ0UG2>
2018-10-05 10:49:33 +0200

    Afegida classe zz_compartit

---
+++ public_html/zz_compartit.java
@@ -0,0 +1,71 @@
+
+/**
+ *
+ * @author fgrange
+ */
+public class zz_compartit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public String getAndres()
+    {
+      }
+    
+    public String getCarlos()
+    {
+      }
+    
+    public String getAdrianV()
+    {
+      }
+    
+    public String getIvan()
+    {
+      }
+    
+    public String getAlex()
+    {
+      }
+    
+    public String getDaniel()
+    {
+      }
+    
+    public String getAlejandro()
+    {
+      }
+    
+    public String getIsmael()
+    {
+      }
+    
+    public String getDemetrio()
+    {
+      }
+    
+    public String getAlbert()
+    {
+      }
+    
+    public String getDenis()
+    {
+      }
+    
+    public String getXavi()
+    {
+      }
+    
+    public String getAlexFreixa()
+    {
+      }
+    
+    public String getIsaam()
+    {
+      }
+    
+}
