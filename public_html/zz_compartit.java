4ca04730baaf421f4b2478a23cd1fe0b5dfe5c6d
AlexCastaneiras <alex.cb98@gmail.com>
2018-10-05 16:51:30 +0200

    Alex: Ultima modificació a l'arxiu compartit

--- public_html/zz_compartit.java
+++ public_html/zz_compartit.java
@@ -35,7 +35,7 @@ public class zz_compartit {
     
     public String getAlex()
     {
-        return "Afegint codi al Ex3 de la prova final";
+        return "Afegint codi al Ex3 de la prova final un altre vegada perque no sortia be el meu usuari.";
       }
     
     public String getDaniel()
